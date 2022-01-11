e757479353a79d7a2b16290cb484aa101d716671
SylwiaLeung <longdepati@gmail.com>
2022-01-11 10:37:50 +0100

    Deleted models from the project and moved to separate library

--- MicroMachines/Models/Dtos/UserReadDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MicroMachines.Models.Dtos
-{
-    public class UserReadDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Funds { get; set; }
-    }
-}
